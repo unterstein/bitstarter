d3710b016cf015b60217bc7a33abcbd4a65a36ac
Johannes Unterstein <unterstein@me.com>
2013-07-08 14:25:55 +0200

    added index.html reading

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+
+app.get('/index.html', function(request, response) {
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
