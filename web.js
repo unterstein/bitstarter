ed871c873e98876b58eb42cb0efa1a66d8b0621b
Johannes Unterstein <unterstein@me.com>
2013-07-15 11:28:55 +0200

    new web.js and more content

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
 
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.configure(function(){
+  app.use(express.static(__dirname + '/'));
 });
 
-app.get('/index.html', function(request, response) {
-  response.send(index.toString());
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
